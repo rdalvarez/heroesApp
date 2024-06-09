0d581c391acd7689fd8f40eb9912de4384f2ee83
David <alv.ricardodavid@gmail.com>
2024-06-09 19:09:07 -0300

    CRUD de Heroes Services EndPoints

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,28 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/enviroment';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
-
   private baseUrl: string = environments.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getHeroes():Observable<Hero[]>{
-    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
-  getHeroById(id: string): Observable<Hero | undefined>{
-    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
-      .pipe(
-        catchError( error => of(undefined) )
-      )
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http
+      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
   }
 
-  getSuggestion( query: string ):Observable<Hero[]>{
-    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`)
+  getSuggestion(query: string): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) {
+      throw Error ('Hero id is required');
+    }
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${ hero.id }`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<Boolean> {
+    return this.http.delete<Hero>(`${this.baseUrl}/heroes/${ id }`)
+    .pipe(
+      catchError( err => of(false)),
+      map( resp => true )
+    );
   }
 }
