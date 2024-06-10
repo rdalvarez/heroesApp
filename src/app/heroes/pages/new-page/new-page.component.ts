55d6a48ceb28a8d36d7c8ee5ef0e5a7a1fa974c5
David <alv.ricardodavid@gmail.com>
2024-06-09 21:08:27 -0300

    Nuevo Getter del componente

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -23,12 +24,25 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
+  constructor ( private heroesService: HeroesService ){}
+
+
+  public get currentHero() : Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+
   onSubmit(){
     console.log(
       { formIsValid: this.heroForm.valid,
         value: this.heroForm.value
       }
     );
+
+    if ( this.heroForm.invalid) return;
+
+    //this.heroesService.updateHero( this.heroForm.value )
 
   }
 
