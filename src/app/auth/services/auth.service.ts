5f30fd8a2a36b7ae11f091aa9d69ef738dda09f6
David <alv.ricardodavid@gmail.com>
2024-07-09 20:33:36 -0300

    Autenticar y mostrar usuario activo

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from '../../../environments/enviroment';
+import { User } from '../interfaces/user.interface';
+import { Observable, filter, tap } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) {}
+
+  get currentUser(): User | undefined {
+    if (!this.user) {
+      return undefined;
+    }
+
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string): Observable<User> {
+    // http.post('login', { email, password });
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => {this.user = user;}),
+      tap((user) => {localStorage.setItem('token', 'asasdwqasd.asdsasdqwasd.asdsqasdwas');})
+    );
+  }
+
+
+  logout(){
+    this.user = undefined;
+    localStorage.clear();
+  }
+}
