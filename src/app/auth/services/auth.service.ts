0211cfdcaec0a3e4ce52cfa469b2b2a54cf7ddf0
David <alv.ricardodavid@gmail.com>
2024-07-09 20:52:55 -0300

    Mantener sessión del usuario

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/enviroment';
 import { User } from '../interfaces/user.interface';
-import { Observable, filter, tap } from 'rxjs';
+import { Observable, catchError, filter, map, of, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -27,6 +27,22 @@ export class AuthService {
     );
   }
 
+  checkAuthentication():Observable<boolean> {
+
+    if ( !localStorage.getItem('token')) {
+      return of(false);
+    }
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl}/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ), // Cuando queremos saber si un objeto tiene un valor le ponemos !! el primer (!) niega el objeto entonces pone false
+        catchError( err => of(false))
+      );
+
+  }
 
   logout(){
     this.user = undefined;
