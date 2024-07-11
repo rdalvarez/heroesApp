3ce1a12278283a5a24fd0f65c8542130085716b1
David <alv.ricardodavid@gmail.com>
2024-07-11 20:35:17 -0300

    Implementacion de funcionalidad del Guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -3,17 +3,38 @@ import {
   CanActivateFn,
   CanMatchFn,
   Route,
+  Router,
   RouterStateSnapshot,
   UrlSegment,
 } from '@angular/router';
+import { inject } from '@angular/core';
+
+import { tap } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+
 
 export const canActivateGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
+
   console.log('CanActivate');
   console.log({ route, state });
-  return true;
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.checkAuthentication()
+    .pipe(
+      tap( isAuthenticated => console.log('Authenticated: ', isAuthenticated)),
+      tap(  isAuthenticated => {
+        if( !isAuthenticated ) {
+          router.navigate(['./auth/login']);
+        }
+      } ),
+
+    )
 };
 
 export const canMatchGuard: CanMatchFn = (
@@ -25,3 +46,7 @@ export const canMatchGuard: CanMatchFn = (
 
   return true;
 };
+
+
+
+
